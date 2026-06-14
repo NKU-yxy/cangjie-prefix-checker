13bd0a9933ebecd72f0e7ac6d9da07029a9e2cdd
eDouFuRu <2640132759@qq.com>
2026-06-14 20:03:10 +0800

    Revert "Local changes before syncing with remote"
    This reverts commit 84ba36a86c74688822ed13850ecb9acc74dc6ea3.

--- third_party/cangjie_typechecker/typechecker/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Cangjie contest-subset parser (Lark)."""
-
-from typechecker.parser import get_parser, parse, parse_file
-from typechecker.parser import strip_leading_package_and_import_lines
-
-__all__ = ["get_parser", "parse", "parse_file", "strip_leading_package_and_import_lines"]
